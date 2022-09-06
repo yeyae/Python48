03d380d07f1478a415cdb6ea47da43d6056fbfaf
yeyae <whyjinny@naver.com>
2022-09-06 18:48:46 +0900

    python

---
+++ 48.py
@@ -0,0 +1,14 @@
+#파이썬에서 class 사용하기
+from unittest import result
+
+
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result += num
+        return self.result
+
+cal1 = Calculator()
+cal2 = Calculator()
